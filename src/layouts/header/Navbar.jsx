e8dfcd58c0c76d5917bbb47c006dc8cd7815de7a
Alvaro Zuna <jcazm92@gmail.com>
2024-11-23 00:30:35 -0400

    location function added

--- src/layouts/header/Navbar.jsx
+++ src/layouts/header/Navbar.jsx
@@ -1,12 +1,13 @@
 
 import { useState, useEffect} from 'react'
 import './styles/Navbar.css'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 const Navbar = () => {
-  
+
   const [isOpen, setIsOpen] = useState(false)
   const [isAtTop, setIsAtTop] = useState(true);
+  const location = useLocation()
 
   useEffect(() => {
     const handleScroll = () => {
@@ -18,43 +19,50 @@ const Navbar = () => {
       window.removeEventListener('scroll', handleScroll)
     }
   }, []);
+
+  //Menu closes after route change detector
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [location])
+  
   
   //Hamburger menu functionality
   const toggleMenu = () => {
     setIsOpen(prevState => !prevState)
   }
 
-  return (
-    <div className = {`navbar ${!isAtTop ? 'navbar--fixed' : ''}`}>
+    return (
+      <div className = {`navbar ${!isAtTop ? 'navbar--fixed' : ''}`}>
         <div className='navbar__logo'>
           <Link to="/">
             <img src="/assets/2 LogoSALUDMED Largo sin fondo.png" alt="Salud Med Logo" />
           </Link>
         </div>
 
-          <nav className='navbar__container'>
-            <ul className={`navbar__menu ${isOpen ? 'navbar__menu--open' : ''}`}>
-              <li className='navbar__item'><Link to="/">Inicio</Link></li>
-                <li className='navbar__item navbar__dropdown'>
-                  <Link>Salud-Med</Link>
-                  <i className='bx bx-chevron-down'></i>
+        <nav className='navbar__container'>
+          <ul className={`navbar__menu ${isOpen ? 'navbar__menu--open' : ''}`}>
+            <li className='navbar__item'><Link to="/">Inicio</Link></li>
+              <li className='navbar__item navbar__dropdown'>
+                <Link>Salud-Med</Link>
+                <i className='bx bx-chevron-down'></i>
 
-                  <ul className='navbar__submenu'>
-                    <li><Link to="/nosotros/quienes-somos">Sobre Nosotros</Link></li>
-                    <li><Link to="/nosotros/mision-vision">Misión Visión</Link></li>
-                    <li><Link to="/nosotros/filosofia-empresarial">Valor Empresarial</Link></li>
-                  </ul>
-                </li>
-              <li className='navbar__item'><Link to="/servicios">Especialidades</Link></li>
-              {/* <li className='navbar__item'><Link to="">Blog</Link></li> */}
-              <li className='navbar__item'><Link to="/contacto">Contáctanos</Link></li>
-            </ul>
-          </nav>
-            <button className='navbar__toggle' onClick={toggleMenu}>
-            <i className='bx bx-menu'></i>
-            </button>
-    </div>
-  )
+                <ul className='navbar__submenu'>
+                  <li><Link to="/nosotros/quienes-somos">Sobre Nosotros</Link></li>
+                  <li><Link to="/nosotros/mision-vision">Misión Visión</Link></li>
+                  <li><Link to="/nosotros/filosofia-empresarial">Valor Empresarial</Link></li>
+                </ul>
+              </li>
+            <li className='navbar__item'><Link to="/servicios">Especialidades</Link></li>
+            {/* <li className='navbar__item'><Link to="">Blog</Link></li> */}
+            <li className='navbar__item'><Link to="/contacto">Contáctanos</Link></li>
+          </ul>
+        </nav>
+        <button className='navbar__toggle' onClick={toggleMenu}>
+          <i className='bx bx-menu'></i>
+        </button>
+      </div>
+    )
 }
 
 export default Navbar
