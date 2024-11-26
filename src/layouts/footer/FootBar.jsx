34331b90fb8127a14b731bee5f883baf96c5d239
Alvaro Zuna <jcazm92@gmail.com>
2024-11-26 11:07:26 -0400

    merge and small fixes

--- src/layouts/footer/FootBar.jsx
+++ src/layouts/footer/FootBar.jsx
@@ -49,8 +49,8 @@ const FootBar = () => {
                 <a href="https://wa.me/56968113358" target='_blank' rel="noopener noreferrer"><i className='bx bxl-whatsapp'></i></a>
                 <a href="mailto:centrosalud@saludmedintegral.com?subject=Consulta&body=Muy%20buenas,%20tengo%20una%20consulta%20sobre..." target='_blank' rel="noopener noreferrer"><i className='bx bx-envelope' ></i></a>
                 <a href="https://www.instagram.com/saludmed_saludmed/" target='_blank' rel="noopener noreferrer"><i className='bx bxl-instagram'></i></a>
-                <a href="#"><i className='bx bxl-youtube'></i></a>
-                <a href="#"><i className='bx bxl-linkedin-square'></i></a>
+                <a href="https://web.facebook.com/profile.php?id=61566551820930" target='_blank' rel='noopener noreferrer'><i className='bx bxl-facebook-circle'></i></a>
+                <a href="#"><i class='bx bxl-spotify' ></i></a>
               </div>
             </div>
     
